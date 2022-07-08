55f3e1201926e50b1cd0297a2406b6280b8f6be8
Daniel Zanchi <danny.zanchi@gmail.com>
2022-07-08 17:07:38 +0200

    first commit

--- Package.swift
+++ Package.swift
@@ -12,15 +12,20 @@ let package = Package(
             targets: ["DZWrapper"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "git@github.com-DZAnalytics:DanielZanchi/DZAnalytics.git", branch: "main"),
+        .package(url: "git@github.com-DZUtilities:DanielZanchi/DZUtilities.git", branch: "main"),
+        .package(url: "git@github.com-DZMonetization:DanielZanchi/DZMonetization.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DZWrapper",
-            dependencies: []),
+            dependencies: [
+                .product(name: "DZUtilities", package: "DZUtilities"),
+                .product(name: "DZDataAnalytics", package: "DZAnalytics"),
+                .product(name: "DZMonetization", package: "DZMonetization")
+            ]),
         .testTarget(
             name: "DZWrapperTests",
             dependencies: ["DZWrapper"]),
